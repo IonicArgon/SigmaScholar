a6f0def5f5d0869cdf2e04d9e7f02af76f9bfe29
Jeniton Augustinpillai <augustinjeniton@gmail.com>
2025-09-13 11:52:04 -0400

    added brainrot detection

--- extension/manifest.config.ts
+++ extension/manifest.config.ts
@@ -21,6 +21,8 @@ export default defineManifest({
   content_scripts: [{
     js: ['src/content/main.tsx'],
     matches: ['https://*/*'],
+    run_at: 'document_end',
+    all_frames: false,
   }],
   content_security_policy: {
     extension_pages: "script-src 'self'; object-src 'self'; connect-src 'self' ws://localhost:* http://localhost:* https://apis.google.com https://www.gstatic.com https://www.googleapis.com https://securetoken.googleapis.com;"
